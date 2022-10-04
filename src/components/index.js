5d45fc8d82e469354b0c915ff38e25bd81a1c4df
Roman Yakobchuk <71780751+RomanYakobchuk@users.noreply.github.com>
2022-10-04 18:15:43 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './NavBar/NavBar'
-export * from './Banner/Banner'
-export * from './Newsletter/Newsletter'
-export * from './Contact/Contact'
-export * from './Footer/Footer'
-export * from './Skills/Skills'
